d02f47277c9fc81a309e33c257eca19e7557d584
Kyle Dewey <kyledewey@cs.ucsb.edu>
2015-01-02 11:48:59 -0800

    Some tweaking to config files.

--- src/main/scala/parfile/config/parfile_config.scala
+++ src/main/scala/parfile/config/parfile_config.scala
@@ -4,9 +4,14 @@ import java.io.File
 
 object StaticSMPConfigFile {
   val NUM_CORES = "num_cores"
+  val CONSUMER_BASE_COMMAND = "consumer_base_command"
+
+  val splitCommand: String => Seq[String] =
+    s => s.split(" ").toSeq
 
   val converters: Map[String, String => Any] =
-    Map(NUM_CORES -> ((s: String) => s.toInt))
+    Map(NUM_CORES -> ((s: String) => s.toInt),
+        CONSUMER_BASE_COMMAND -> splitCommand)
 
   val defaults: Map[String, Any] =
     // Specifiy 0 for the same number as the system has
@@ -14,37 +19,35 @@ object StaticSMPConfigFile {
 }
 
 trait NumCores extends ConfigFileInterface {
-  import StaticSMPConfigFile._
-  def numCores(): Int = getEntry(NUM_CORES).asInstanceOf[Int]
+  def numCores(): Int =
+    getEntry(StaticSMPConfigFile.NUM_CORES).asInstanceOf[Int]
+}
+
+trait ConsumerBaseCommand extends ConfigFileInterface {
+  def consumerBaseCommand(): Seq[String] =
+    getEntry(StaticSMPConfigFile.CONSUMER_BASE_COMMAND).asInstanceOf[Seq[String]]
 }
 
 class StaticSMPConfigFile(file: File)
-extends ConfigFile(file, StaticSMPConfigFile.converters, StaticSMPConfigFile.defaults) with NumCores
+extends ConfigFile(file, StaticSMPConfigFile.converters, StaticSMPConfigFile.defaults) with NumCores with ConsumerBaseCommand
 
 object DynamicSMPConfigFile {
-  import ConfigFile.id
-
   val PRODUCER_COMMAND = "producer_command"
-  val CONSUMER_BASE_COMMAND = "consumer_base_command"
   val CONSUMER_SAVE_FILE_TO = "consumer_save_file_to"
 
-  val splitCommand: String => Seq[String] =
-    s => s.split(" ").toSeq
-
   val converters: Map[String, String => Any] =
-    Map(PRODUCER_COMMAND -> splitCommand,
-        CONSUMER_BASE_COMMAND -> splitCommand,
-        CONSUMER_SAVE_FILE_TO -> id) ++ StaticSMPConfigFile.converters
+    Map(PRODUCER_COMMAND -> StaticSMPConfigFile.splitCommand,
+        CONSUMER_SAVE_FILE_TO -> ConfigFile.id) ++ StaticSMPConfigFile.converters
 
   val defaults: Map[String, Any] = StaticSMPConfigFile.defaults
 }
 
 class DynamicSMPConfigFile(file: File)
-extends ConfigFile(file, DynamicSMPConfigFile.converters, DynamicSMPConfigFile.defaults) with NumCores {
+extends ConfigFile(file, DynamicSMPConfigFile.converters, DynamicSMPConfigFile.defaults) with NumCores with ConsumerBaseCommand {
   import DynamicSMPConfigFile._
   def producerCommand(): Seq[String] =
     getEntry(PRODUCER_COMMAND).asInstanceOf[Seq[String]]
 
-  def consumerBaseCommand(): Seq[String] =
-    getEntry(CONSUMER_BASE_COMMAND).asInstanceOf[Seq[String]]
+  def consumerSaveFileTo(): String =
+    getEntry(CONSUMER_SAVE_FILE_TO).asInstanceOf[String]
 }
